17fd60cd8fa5e908bb9554e22ec993f6d108e61a
Tomasz Put <tomasz.put@blockfi.com>
2023-10-23 11:54:02 +0200

    Create empty SpringBoot app

---
+++ src/main/java/com/jamcito/inpost/productsrest/ProductsrestApplication.java
@@ -0,0 +1,13 @@
+package com.jamcito.inpost.productsrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductsRESTApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductsRESTApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jamcito/inpost/productsrest/ProductsrestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jamcito.inpost.productsrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsrestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
