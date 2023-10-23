1ac670efa679ad324a3bd02061fb7ac3436f7934
Tomasz Put <tomasz.put@blockfi.com>
2023-10-23 13:01:55 +0200

    Implement HAL style links to make API RESTful

---
+++ src/main/java/com/jamcito/inpost/productsrest/shopping/ProductModelAssembler.java
@@ -0,0 +1,19 @@
+package com.jamcito.inpost.productsrest.shopping;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.stereotype.Component;
+
+@Component
+class ProductModelAssembler implements RepresentationModelAssembler<Product, EntityModel<Product>> {
+
+  @Override
+  public EntityModel<Product> toModel(Product product) {
+
+    return EntityModel.of(product,
+    linkTo(methodOn(ShoppingController.class).getProduct(product.getId())).withSelfRel(),
+    linkTo(methodOn(ShoppingController.class).getAllProducts()).withRel("products"));
+  }
+}
--- src/main/java/com/jamcito/inpost/productsrest/shopping/ShoppingController.java
+++ src/main/java/com/jamcito/inpost/productsrest/shopping/ShoppingController.java
@@ -5,39 +5,62 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
+
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.IanaLinkRelations;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+
 @RestController
 public class ShoppingController {
+
+    private final ProductModelAssembler productAssembler;
     private final ProductRepository productRepository;
 
-    ShoppingController(ProductRepository productRepository) {
+    ShoppingController(ProductModelAssembler productAssembler, ProductRepository productRepository) {
+        this.productAssembler = productAssembler;
         this.productRepository = productRepository;
     }
 
     @GetMapping("/products")
-    List<Product> getAllProducts() {
-        return productRepository.findAll();
+    ResponseEntity<CollectionModel<EntityModel<Product>>> getAllProducts() {
+        List<EntityModel<Product>> products = productRepository.findAll().stream()
+                .map(productAssembler::toModel)
+                .collect(Collectors.toList());
+
+        return ResponseEntity
+                .ok(CollectionModel.of(products,
+                        linkTo(methodOn(ShoppingController.class).getAllProducts()).withSelfRel()));
     }
 
     @PostMapping("/products")
-    Product postProduct(@RequestBody Product product) {
-        return productRepository.save(product);
+    ResponseEntity<EntityModel<Product>> postProduct(@RequestBody Product product) {
+        EntityModel<Product> entityModel = productAssembler.toModel(productRepository.save(product));
+
+        return ResponseEntity
+                .created(entityModel.getRequiredLink(IanaLinkRelations.SELF).toUri())
+                .body(entityModel);
     }
 
     @GetMapping("/products/{id}")
-    Product getProduct(@PathVariable UUID id) {
-        return productRepository.findById(id)
+    ResponseEntity<EntityModel<Product>> getProduct(@PathVariable UUID id) {
+        Product product = productRepository.findById(id)
                 .orElseThrow(() -> new ProductNotFoundException(id));
+
+        return ResponseEntity.ok(productAssembler.toModel(product));
     }
 
     @PutMapping("/products/{id}")
-    Product putProduct(@RequestBody Product product, @PathVariable UUID id) {
-        return productRepository.findById(id)
+    ResponseEntity<EntityModel<Product>> putProduct(@RequestBody Product product, @PathVariable UUID id) {
+        Product updatedProduct = productRepository.findById(id)
                 .map(oldproduct -> {
                     oldproduct.setCount(product.getCount());
                     oldproduct.setPrice(product.getPrice());
@@ -47,6 +70,7 @@ public class ShoppingController {
                     product.setId(id);
                     return productRepository.save(product);
                 });
+        return ResponseEntity.ok(productAssembler.toModel(updatedProduct));
     }
 
     @DeleteMapping("/products/{id}")
