971c9ba57d9bc403353c8a9fa33d625e01256a67
Tomasz Put <tomasz.put@blockfi.com>
2023-10-23 12:36:35 +0200

    Preload sample data

---
+++ src/main/java/com/jamcito/inpost/productsrest/shopping/ProductLoader.java
@@ -0,0 +1,26 @@
+package com.jamcito.inpost.productsrest.shopping;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ProductLoader {
+
+    public class LoadProducts {
+
+        private static final Logger log = LoggerFactory.getLogger(LoadProducts.class);
+
+        @Bean
+        CommandLineRunner initDatabase(ProductRepository repository) {
+
+            return args -> {
+                log.info("Inserting " + repository.save(new Product(10, 1000)));
+                log.info("Inserting " + repository.save(new Product(20, 175)));
+                log.info("Inserting " + repository.save(new Product(50, 300)));
+            };
+        }
+    }
+}
