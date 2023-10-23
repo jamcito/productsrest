085eb0350976fe7e66ee30f41c56b74429ea5d63
Tomasz Put <tomasz.put@blockfi.com>
2023-10-23 13:07:43 +0200

    Add discount policy to product entity

---
+++ src/main/java/com/jamcito/inpost/productsrest/shopping/DiscountPolicy.java
@@ -0,0 +1,5 @@
+package com.jamcito.inpost.productsrest.shopping;
+
+public enum DiscountPolicy {
+    NO_DISCOUNT, FLAT;
+}
--- src/main/java/com/jamcito/inpost/productsrest/shopping/Product.java
+++ src/main/java/com/jamcito/inpost/productsrest/shopping/Product.java
@@ -13,13 +13,19 @@ public class Product {
     private @Id @GeneratedValue UUID id;
     private Integer count;
     private Integer price;
+    private DiscountPolicy policy;
 
     public Product() {
     }
 
     public Product(Integer count, Integer price) {
+        this(count, price, DiscountPolicy.NO_DISCOUNT);
+    }
+
+    public Product(Integer count, Integer price, DiscountPolicy policy) {
         this.count = count;
         this.price = price;
+        this.policy = policy;
     }
 
     public UUID getId() {
@@ -34,6 +40,10 @@ public class Product {
         return this.price;
     }
 
+    public DiscountPolicy getPolicy() {
+        return this.policy;
+    }
+
     public void setId(UUID id) {
         this.id = id;
     }
@@ -46,6 +56,10 @@ public class Product {
         this.price = price;
     }
 
+    public void setPolicy(DiscountPolicy policy) {
+        this.policy = policy;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o)
@@ -55,16 +69,18 @@ public class Product {
         Product product = (Product) o;
         return Objects.equals(this.id, product.id)
                 && Objects.equals(this.count, product.count)
-                && Objects.equals(this.price, product.price);
+                && Objects.equals(this.price, product.price)
+                && Objects.equals(this.policy, product.policy);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(this.id, this.count, this.price);
+        return Objects.hash(this.id, this.count, this.price, this.policy);
     }
 
     @Override
     public String toString() {
-        return "Product{id=" + this.id + ", count=" + this.count + ", price=" + this.price + "}";
+        return "Product{id=" + this.id + ", count=" + this.count + ", price=" + this.price + ", discountPolicy="
+                + this.policy + "}";
     }
 }
